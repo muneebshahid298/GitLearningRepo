7bc1549a6c7e22799e4fcb5d9da7683235188210
saurabhsmasher <91626327+saurabhsmasher@users.noreply.github.com>
2022-01-02 20:38:08 +0530

    initial commit

---
+++ loops.java
@@ -0,0 +1,20 @@
+package trying;
+import java.io.*;
+import java.util.*;
+public class Loops {
+
+	
+	
+	public static void main (String [] args){
+
+	int i,n;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the number of times");
+	n=sc.nextInt();
+
+
+	for( i=0;i<n;i++){
+	System.out.println("my name is saurabh tiwari");
+	}
+	}
+	}
